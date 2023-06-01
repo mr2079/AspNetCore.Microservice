a086111430c65f2348a37cac316c46120fbef284
mr2079 <mohammadalikhoshdooni@yahoo.com>
2023-06-01 10:28:24 +0330

    Create basket api -> redis

---
+++ Services/Basket/Basket.Api/Entities/Cart.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.Api.Entities;
+
+public class Cart
+{
+    public Cart() {}
+    public Cart(string userName) { UserName = userName; }
+
+    public string UserName { get; set; } = string.Empty;
+    public List<CartItem>? Items { get; set; }
+
+    public decimal TotalPrice
+    {
+        get
+        {
+            decimal totalPrice = 0;
+            foreach (CartItem item in Items)
+                totalPrice += item.Price * item.Quantity;
+            return totalPrice;
+        }
+    }
+}
+ 
---
+++ Services/Basket/Basket.Api/Entities/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.Api.Entities;
+
+public class CartItem
+{
+    public string ProductId { get; set; } = string.Empty;
+    public string ProductName { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
