f9aa8d2daaff56958b2ca50d22129f1acc993781
mr2079 <mohammadalikhoshdooni@yahoo.com>
2023-06-01 10:56:01 +0330

    Basket api basket repository

---
+++ Services/Basket/Basket.Api/Repositories/BasketRepository.cs
@@ -0,0 +1,35 @@
+﻿using Basket.Api.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.Api.Repositories;
+
+public class BasketRepository : IBasketRepository
+{
+    private readonly IDistributedCache _cache;
+
+    public BasketRepository(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<Cart> GetUserBasket(string userName)
+    {
+        string basket = await _cache.GetStringAsync(userName);
+        if (string.IsNullOrEmpty(basket)) return new();
+
+        return JsonConvert.DeserializeObject<Cart>(basket) ?? new();
+    }
+
+    public async Task<Cart> UpsertBasket(Cart basket)
+    {
+        await _cache.SetStringAsync(basket.UserName,
+            JsonConvert.SerializeObject(basket));
+
+        return await GetUserBasket(basket.UserName);
+    }
+    public async Task DeleteBasket(string userName)
+    {
+       await _cache.RemoveAsync(userName);
+    }
+}
---
+++ Services/Basket/Basket.Api/Repositories/IBasketRepository.cs
@@ -0,0 +1,10 @@
+﻿using Basket.Api.Entities;
+
+namespace Basket.Api.Repositories;
+
+public interface IBasketRepository
+{
+    Task<Cart> GetUserBasket(string userName);
+    Task<Cart> UpsertBasket(Cart basket);
+    Task DeleteBasket(string userName);
+}
