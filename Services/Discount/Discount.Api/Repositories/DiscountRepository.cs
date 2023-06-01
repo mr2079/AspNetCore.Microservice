a19c0997fccf8ec670d9600be3bf0861cb0b47e6
mr2079 <mohammadalikhoshdooni@yahoo.com>
2023-06-01 20:43:52 +0330

    Discount api discount repository

---
+++ Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using Discount.Api.Entities;
+using Npgsql;
+
+namespace Discount.Api.Repositories;
+
+public class DiscountRepository : IDiscountRepository
+{
+    private readonly IConfiguration _configuration;
+
+    public DiscountRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<Coupon> GetDiscount(int id)
+    {
+        using var connection = new NpgsqlConnection(
+            _configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+        var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>(
+            "SELECT * FROM Coupon WHERE Id=@Id", new { Id = id });
+
+        if (coupon == null) return new Coupon()
+        { ProductName = "No discount", Description = "No discount description", Amount = 0 };
+
+        return coupon;
+    }
+
+    public async Task<bool> CreateDiscount(Coupon coupon)
+    {
+        using var connection = new NpgsqlConnection(
+            _configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+        var affected = await connection.ExecuteAsync(
+            "INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
+            new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+
+        if (affected == 0) return false;
+
+        return true;
+    }
+
+    public async Task<bool> UpdateDiscount(Coupon coupon)
+    {
+        using var connection = new NpgsqlConnection(
+            _configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+        var affected = await connection.ExecuteAsync(
+            "UPDATE Coupon SET ProductName=@ProductName, Description=@Description, Amount=@Amount " +
+            "WHERE Id=@Id",
+            new { Id = coupon.Id,
+                ProductName = coupon.ProductName,
+                Description = coupon.Description,
+                Amount = coupon.Amount });
+
+        if (affected == 0) return false;
+
+        return true;
+    }
+
+    public async Task<bool> DeleteDiscount(int id)
+    {
+        using var connection = new NpgsqlConnection(
+            _configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+        var affected = await connection.ExecuteAsync(
+            "DELETE FROM Coupon WHERE Id=@Id", new { Id = id });
+
+        if (affected == 0) return false;
+
+        return true;
+    }
+}
---
+++ Services/Discount/Discount.Api/Repositories/IDiscountRepository.cs
@@ -0,0 +1,11 @@
+﻿using Discount.Api.Entities;
+
+namespace Discount.Api.Repositories;
+
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscount(int id);
+    Task<bool> CreateDiscount(Coupon coupon);
+    Task<bool> UpdateDiscount(Coupon coupon);
+    Task<bool> DeleteDiscount(int id);
+}
