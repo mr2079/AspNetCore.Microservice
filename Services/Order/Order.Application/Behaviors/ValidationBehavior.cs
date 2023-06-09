29d47bfc8a713aaa3be036ad85e19fdd619a8432
mr2079 <mohammadalikhoshdooni@yahoo.com>
2023-06-09 15:24:37 +0330

    Order api validation and exception behaviors

--- Services/Order/Order.Application/Behaviors/ValidationBehavior.cs
+++ Services/Order/Order.Application/Behaviors/ValidationBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using MediatR;
+using ValidationException =  Order.Application.Exceptions.ValidationException;
 
 namespace Order.Application.Behaviors;
 
@@ -12,11 +13,21 @@ public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReques
         _validators = validators;
     }
 
-    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (_validators.Any())
         {
-            
+            var context = new ValidationContext<TRequest>(request);
+
+            // Main validation operation
+            var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+            var failures = validationResults.SelectMany(r => r.Errors).Where(a => a != null).ToList();
+
+            if (failures.Count > 0)
+                throw new ValidationException(failures);
         }
+
+        return await next();
     }
 }
