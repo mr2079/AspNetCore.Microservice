1613f1bdc414ebb238e9a7e84b1dcb66f8e7ba96
mr2079 <mohammadalikhoshdooni@yahoo.com>
2023-06-09 15:46:23 +0330

    Order api application layer ioc manager

---
+++ Services/Order/Order.Application/IoC/ApplicationServicesRegistration.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Order.Application.Behaviors;
+using System.Reflection;
+
+namespace Order.Application.IoC;
+
+public static class ApplicationServicesRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
